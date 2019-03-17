6c3c04dd7ec9fcf2853c13f22c9d3fb056cc824d
D.M.J. Tax <davidt@MacBook-Pro.fritz.box>
2019-03-17 10:22:20 +0100

    Slightly extended the example.

--- prex_plotc.py
+++ prex_plotc.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 
 # standard prtools: make data, train classifier, plot:
 a = gendatb((40,40))
-u = prmapping(proxm,'rbf',1.5)*prmapping(nmc)
+u = scalem()*proxm(('rbf',0.5))*nmc()
 w = a*u
+out = a*w
+err = out*testc()
+print('Classification error: %3.2f' % err)
 plt.figure(1)
 scatterd(a)
 plotc(w)
