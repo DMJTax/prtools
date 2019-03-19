f3fb71a63096f11f6c842a8c2d418c19b6a077dd
D.M.J. Tax <davidt@tud0035369.ws.tudelft.net>
2019-03-19 08:51:19 +0100

    More complete example script.

--- prex_plotc.py
+++ prex_plotc.py
@@ -1,15 +1,28 @@
 from prtools import *
 import matplotlib.pyplot as plt
 
-# standard prtools: make data, train classifier, plot:
-a = gendatb((40,40))
-u = scalem()*proxm(('rbf',0.5))*nmc()
+# standard prtools: make data and define classifier:
+#a = gendatb((30,30))
+#a = gendatd((30,30))
+a = gendath((30,30))
+#a = gendats3((100))
+
+#u = nmc()
+#u = scalem()*proxm(('rbf',0.5))*nmc()
+u = gaussm(('full',0.1))*bayesrule()
+#u = qdc()
+#u = knnc([1])
+#u = scalem()*parzenc()
+
+# train classifier, plot:
 w = a*u
-out = a*w
-err = out*testc()
+pred = a*w
+print w
+err = pred*testc()
 print('Classification error: %3.2f' % err)
 plt.figure(1)
 scatterd(a)
 plotc(w)
+#plotm(w,gridsize=100,colors='gray')
 
 plt.show()
