00ce64da8f880507f2c6bcb9f3095fceee95c4e3
D.M.J. Tax <davidt@MacBook-Pro.fritz.box>
2019-03-27 19:36:33 +0100

    Splitting in files was a bad idea.

--- prtools.py
+++ prtools.py
@@ -139,7 +139,7 @@ class prmapping(object):
 
 def sequentialm(task=None,x=None,w=None):
     "Sequential mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         # we should have gotten a list of two prmappings
         if not isinstance(task,tuple):
             raise ValueError('Sequential map expects a list of 2 prmappings.')
@@ -257,7 +257,7 @@ def plotm(f,nrlevels=10,colors=None,gridsize = 30):
 
 def scalem(task=None,x=None,w=None):
     "Scale mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         return prmapping(scalem,task,x)
     if (task=='untrained'):
         # just return the name, and hyperparameters
@@ -280,7 +280,7 @@ def scalem(task=None,x=None,w=None):
 
 def proxm(task=None,x=None,w=None):
     "Proximity mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         # A direct call to proxm, refer back to prmapping:
         return prmapping(proxm,task,x)
     if (task=='untrained'):
@@ -336,7 +336,7 @@ def proxm(task=None,x=None,w=None):
 
 def softmax(task=None,x=None,w=None):
     "Softmax mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(softmax)
         out.mapping_type = "trained"
         if task is not None:
@@ -361,7 +361,7 @@ def softmax(task=None,x=None,w=None):
 
 def classc(task=None,x=None,w=None):
     "Classc mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(classc)
         out.mapping_type = "trained"
         if task is not None:
@@ -388,7 +388,7 @@ def classc(task=None,x=None,w=None):
 
 def labeld(task=None,x=None,w=None):
     "Label mapping"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(labeld)
         out.mapping_type = "trained"
         if task is not None:
@@ -414,7 +414,7 @@ def labeld(task=None,x=None,w=None):
 
 def testc(task=None,x=None,w=None):
     "Test classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(testc)
         out.mapping_type = "trained"
         return out
@@ -436,7 +436,7 @@ def testc(task=None,x=None,w=None):
 
 def mclassc(task=None,x=None,w=None):
     "Multiclass classifier from two-class classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(mclassc,task,x)
         return out
     if (task=='untrained'):
@@ -477,7 +477,7 @@ def mclassc(task=None,x=None,w=None):
 
 def bayesrule(task=None,x=None,w=None):
     "Bayesrule"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(bayesrule)
         out.mapping_type = "trained"
         return out
@@ -503,7 +503,7 @@ def bayesrule(task=None,x=None,w=None):
 
 def gaussm(task=None,x=None,w=None):
     "Gaussian density"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(gaussm,task,x)
         return out
     if (task=='untrained'):
@@ -581,7 +581,7 @@ def qdc(task=None,x=None,w=None):
 
 def nmc(task=None,x=None,w=None):
     "Nearest mean classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(nmc,task,x)
         return out
     if (task=='untrained'):
@@ -607,7 +607,7 @@ def nmc(task=None,x=None,w=None):
 
 def fisherc(task=None,x=None,w=None):
     "Fisher classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(fisherc,task,x)
         return out
     if (task=='untrained'):
@@ -654,7 +654,7 @@ def fisherc(task=None,x=None,w=None):
 
 def knnm(task=None,x=None,w=None):
     "k-Nearest neighbor classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(knnm,task,x)
         return out
     if (task=='untrained'):
@@ -690,7 +690,7 @@ def knnc(task=None,x=None,w=None):
 
 def parzenm(task=None,x=None,w=None):
     "Parzen density estimate per class"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(parzenm,task,x)
         return out
     if (task=='untrained'):
@@ -724,7 +724,7 @@ def parzenc(task=None,x=None,w=None):
 
 def naivebm(task=None,x=None,w=None):
     "Naive Bayes density estimate"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(naivebm,task,x)
         return out
     if (task=='untrained'):
@@ -762,7 +762,7 @@ def naivebc(task=None,x=None,w=None):
 
 def baggingc(task=None,x=None,w=None):
     "Bagging"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(baggingc,task,x)
         return out
     if (task=='untrained'):
@@ -803,7 +803,7 @@ def baggingc(task=None,x=None,w=None):
 
 def stumpc(task=None,x=None,w=None):
     "Decision stump classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(stumpc,task,x)
         return out
     if (task=='untrained'):
@@ -886,7 +886,7 @@ def stumpc(task=None,x=None,w=None):
 
 def adaboostc(task=None,x=None,w=None):
     "AdaBoost classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(adaboostc,task,x)
         return out
     if (task=='untrained'):
@@ -950,7 +950,7 @@ def adaboostc(task=None,x=None,w=None):
 
 def svc(task=None,x=None,w=None):
     "Support vector classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(svc,task,x)
         return out
     if (task=='untrained'):
@@ -989,7 +989,7 @@ def svc(task=None,x=None,w=None):
 
 def dectreec(task=None,x=None,w=None):
     "Decision tree classifier"
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(dectreec,task,x)
         return out
     if (task=='untrained'):
@@ -1021,7 +1021,7 @@ def dectreec(task=None,x=None,w=None):
 
 def pcam(task=None,x=None,w=None):
     "Principal Component Analysis "
-    if not isinstance(task,basestring):
+    if not isinstance(task,str):
         out = prmapping(pcam,task,x)
         return out
     if (task=='untrained'):
@@ -1119,7 +1119,7 @@ def clevalf(a,u,trainsize=0.6,nrreps=5):
 #
 def m2p(f,*args):
     "ML to PRtools mapping"
-    if isinstance(f,basestring):
+    if isinstance(f,str):
         if (f=='untrained'):
             return 'M2P '
     if isinstance(f,mlearn.mlmodel):
@@ -1140,4 +1140,349 @@ def m2p(f,*args):
             print("we did not get a ml model!!")
 
         return out[0]
+
+
+# === prdataset ============================================
+class prdataset(object):
+    "Prdataset in python"
+
+    def __init__(self,data,labels=None):
+        if isinstance(data,prdataset):
+            #self = copy.deepcopy(data) # why does this not work??
+            self.__dict__ = data.__dict__.copy()   # sigh..
+            return
+        if not isinstance(data,(numpy.ndarray, numpy.generic)):
+            data = numpy.array(data,ndmin=2)
+            if not isinstance(data,(numpy.ndarray, numpy.generic)):
+                raise ValueError('Data matrix should be a numpy matrix.')
+        if labels is not None:
+            if not isinstance(labels,(numpy.ndarray, numpy.generic)):
+                raise ValueError('Label vector should be a numpy matrix.')
+            if (data.shape[0]!=labels.shape[0]):
+                raise ValueError('Number of labels does not match number of data samples.')
+        else:
+            labels = numpy.zeros(data.shape[0])
+        self.name = ''
+        self.featlab = numpy.arange(data.shape[1])
+        self.setdata(data)
+        self.labels = labels
+        self.weights = []
+        self.prior = []
+        self.user = []
+
+    def __str__(self):
+        sz = self.data.shape
+        if (len(self.name)>0):
+            outstr = "%s %d by %d prdataset" % (self.name,sz[0],sz[1])
+        else:
+            outstr = "%d by %d prdataset" % (sz[0],sz[1])
+        cnt = self.classsizes()
+        nrcl = len(cnt)
+        if (nrcl==0):
+            outstr += " with no labels"
+        elif (nrcl==1):
+            outstr += " with 1 class: [%d]"%sz[0]
+        else:
+            outstr += " with %d classes: [%d"%(nrcl,cnt[0])
+            for i in range(1,nrcl):
+                outstr += " %d"%cnt[i]
+            outstr += "]"
+        return outstr
+
+    def float(self):
+        return self.data.copy()
+    def __pos__(self):
+        return self.float()
+    def __add__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data += other
+        return newd
+    def __sub__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data -= other
+        return newd
+    def __mul__(self,other):
+        #print('prdataset multiplication with right')
+        #print('   self='+str(self))
+        #print('   other='+str(other))
+
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prmapping)):
+            newother = copy.deepcopy(other)
+            out = newother(newd)
+            return out
+        elif (isinstance(other,prdataset)):
+            other = other.float()
+        else:
+            raise ValueError("Cannot multiply a prdataset with RHS.")
+        newd.data *= other
+        return newd
+    def __div__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data /= other
+        return newd
+
+    def lablist(self):
+        return numpy.unique(self.labels)
+    def nlab(self):
+        (ll,I) = numpy.unique(self.labels,return_inverse=True)
+        I = numpy.array(I)
+        I = I[:,numpy.newaxis] # python is so terrible..:-(
+        return I
+    def signlab(self,posclass=1):  # what is a good default?
+        ll = self.lablist()
+        if (len(ll)>2):
+            raise ValueError('Labels +-1 only for two-class problems.')
+        lab = self.nlab()
+        if (posclass==0):
+            lab = 1-2.0*lab
+        else:
+            lab = 2.0*lab-1
+        return lab
+    def setdata(self,data):
+        self.data = data
+        self.shape = data.shape
+        if len(self.featlab) != data.shape[1]:
+            self.featlab = ['Feature 0']
+            for i in range(1,data.shape[1]):
+                self.featlab.append('Feature %d'%i)
+        return self
+
+    def classsizes(self):
+        try:
+            (k,count) = numpy.unique(self.labels,return_counts=True)
+        except:
+            ll = numpy.unique(self.labels)
+            count = numpy.zeros((len(ll),1))
+            for i in range(len(ll)):
+                count[i] = numpy.sum(1.*(self.labels==ll[i]))
+        return count
+    def nrclasses(self):
+        ll = numpy.unique(self.labels)
+        return len(ll)
+    def findclass(self,cname):
+        ll = numpy.unique(self.labels)
+        I = numpy.where(ll==cname)
+        return I[0][0]
+
+    def __getitem__(self,key):
+        newd = copy.deepcopy(self)
+        k1 = key[0]
+        k2 = key[1]
+        if isinstance(k1,int):  # fucking python!
+            k1 = range(k1,k1+1)
+        if isinstance(k2,int):  # fucking python!!!!
+            k2 = range(k2,k2+1)
+        newkey = (k1,k2)
+        newfeatlab = newd.featlab[key[1]]
+        if not isinstance(newfeatlab,numpy.ndarray): #DXD why??
+            newfeatlab = numpy.array([newfeatlab]) # GRR Python
+        newd.featlab = newfeatlab
+        newd = newd.setdata(newd.data[newkey])
+        newd.labels = newd.labels[newkey[0]]
+        return newd
+
+    def seldat(self,cl):
+        newd = copy.deepcopy(self)
+        #I = (self.labels==cl).nonzero()
+        I = (self.nlab()==cl).nonzero()
+        return newd[I[0],:]   # grr, this python..
+
+    def getprior(self):
+        if (len(self.prior)>0) and (self.prior>0):
+            return self.prior
+        sz = self.classsizes()
+        return sz/float(numpy.sum(sz))
+
+    def concatenate(self,other):
+        out = copy.deepcopy(self)
+        out = out.setdata(numpy.concatenate((out.data,other.data),axis=0))
+        out.labels = numpy.concatenate((out.labels,other.labels),axis=0)
+        return out
+
+
+
+
+# === useful functions =====================================
+def scatterd(a):
+    clrs = a.nlab().flatten()
+    sz = a.data.shape
+    if (sz[1]>1):
+        plt.scatter(a.data[:,0],a.data[:,1],c=clrs)
+        ylab = a.featlab[1]
+    else:
+        plt.scatter(a.data[:,0],numpy.zeros((sz[0],1)),c=clrs)
+        ylab = ''
+    plt.title(a.name)
+    plt.xlabel('Feature '+str(a.featlab[0]))
+    plt.ylabel('Feature '+str(ylab))
+    plt.winter()
+
+# === datasets ===============================
+def genclass(n,p):
+    "Generate class frequency distribution"
+    if isinstance(n,int):
+        n = [n]  # make a list
+    n = numpy.asarray(n)
+    if (len(n)>1):
+        return n
+    if (len(n.shape)>1):
+        return n
+    p = numpy.asarray(p)
+    if (numpy.abs(numpy.sum(p)-1)>1e-9):
+        raise ValueError('Probabilities do not add up to 1.')
+    c = len(p)
+    P = numpy.cumsum(p)
+    P = numpy.concatenate((numpy.zeros(1),P)) # I hate python
+    z = numpy.zeros(c,dtype=int)
+    x = numpy.random.rand(numpy.sum(n))
+    for i in range(0,c):
+        z[i] = numpy.sum((x>P[i]) & (x<P[i+1]))
+    return z
+
+def genlab(n,lab):
+    if (len(n)!=len(lab)):
+        raise ErrorValue('Number of values in N should match number in lab')
+    out = numpy.tile(lab[0],[n[0],1])
+    for i in range(1,len(n)):
+        out=numpy.concatenate((out,numpy.tile(lab[i],[n[i],1])))
+    return out
+
+def gendat(x,n,seed=None):
+    nrcl = x.nrclasses()
+    clsz = x.classsizes()
+    prior = x.getprior()
+    # and all the casting:
+    if isinstance(n,float):  # we start with a fraction
+        n = n*clsz
+    if isinstance(n,int):    # we start with a total number
+        n = genclass(n,prior)
+    if isinstance(n,tuple):
+        n = list(n)
+    if isinstance(n[0],float) and (n[0]<1.): # a vector/list of fractions
+        n = n*clsz
+    # take care for the seed:
+    numpy.random.seed(seed)
+    # now generate the data:
+    i=0  # first class is special:
+    x1 = x.seldat(i)
+    if (n[i]==clsz[i]):
+        # take a bootstrap sample:
+        I = numpy.random.randint(0,n[i],n[i])
+    elif (n[i]<clsz[i]):
+        I = numpy.random.permutation(range(clsz[i]))
+        I = I[0:int(n[i])]
+    else:
+        I = numpy.random.randint(clsz[i],size=int(n[i]))
+    out = x1[I,:]
+    allI = range(clsz[i])
+    J = numpy.setdiff1d(allI,I)
+    leftout = x1[J,:]
+    # now the other classes:
+    for i in range(1,nrcl):
+        xi = x.seldat(i)
+        if (n[i]==clsz[i]):
+            # take a bootstrap sample:
+            I = numpy.random.randint(0,n[i],n[i])
+        elif (n[i]<clsz[i]):
+            I = numpy.random.permutation(range(clsz[i]))
+            I = I[0:int(n[i])]
+        else:
+            I = numpy.random.randint(clsz[i],size=int(n[i]))
+        out = out.concatenate(xi[I,:])
+        allI = range(clsz[i])
+        J = numpy.setdiff1d(allI,I)
+        leftout = leftout.concatenate(xi[J,:])
+
+    return out,leftout
+
+def gendats(n,dim=2,delta=2.):
+    prior = [0.5,0.5]
+    N = genclass(n,prior)
+    x0 = numpy.random.randn(N[0],dim)
+    x1 = numpy.random.randn(N[1],dim)
+    x1[:,0] = x1[:,0] + delta  # move data from class 1
+    x = numpy.concatenate((x0,x1),axis=0)
+    y = genlab(N,(-1,1))
+    out = prdataset(x,y)
+    out.name = 'Simple dataset'
+    out.prior = prior
+    return out
+
+def gendatd(n,dim=2,d1=2.,d2=1.):
+    prior = [0.5,0.5]
+    N = genclass(n,prior)
+    x0 = numpy.random.randn(N[0],dim)
+    x1 = numpy.random.randn(N[1],dim)
+    x0[:,1:] *= numpy.sqrt(40)
+    x1[:,1:] *= numpy.sqrt(40)
+    x1[:,0] += d1  # move data from class 1
+    x1[:,1] += d1  # move data from class 1
+    x = numpy.concatenate((x0,x1),axis=0)
+    R = numpy.array([[1.,-1.],[1.,1.]])
+    x[:,0:2] = x[:,0:2].dot(R)
+    y = genlab(N,(-1,1))
+    out = prdataset(x,y)
+    out.name = 'Difficult dataset'
+    out.prior = prior
+    return out
+
+def gendatb(n,s=1.0):
+    r = 5
+    prior = [0.5,0.5]
+    N = genclass(n,prior)
+    domaina = 0.125*numpy.pi + 1.25*numpy.pi*numpy.random.rand(N[0],1)
+    a = numpy.concatenate((r*numpy.sin(domaina),r*numpy.cos(domaina)),axis=1)
+    a += s*numpy.random.randn(N[0],2)
+
+    domainb = 0.375*numpy.pi - 1.25*numpy.pi*numpy.random.rand(N[1],1)
+    b = numpy.concatenate((r*numpy.sin(domainb),r*numpy.cos(domainb)),axis=1)
+    b += s*numpy.random.randn(N[1],2)
+    b -= 0.75*r*numpy.ones((N[1],2))
+
+    x = numpy.concatenate((a,b),axis=0)
+    y = genlab(N,(-1,1))
+    out = prdataset(x,y)
+    out.name = 'Banana dataset'
+    out.prior = prior
+    return out
+
+def gendath(n):
+    prior = [0.5,0.5]
+    N = genclass(n,prior)
+    x0 = numpy.random.randn(N[0],2)
+    x0[:,0] = x0[:,0] + 1.     # feature 0 from class 0
+    x0[:,1] = 0.5*x0[:,1] + 1  # feature 1 from class 0
+    x1 = numpy.random.randn(N[1],2)
+    x1[:,0] = 0.1*x1[:,0] + 2. # feature 0 from class 1
+    x1[:,1] = 2.*x1[:,1]       # feature 1 from class 1
+    x = numpy.concatenate((x0,x1),axis=0)
+    y = genlab(N,(-1,1))
+    out = prdataset(x,y)
+    out.name = 'Highleyman dataset'
+    out.prior = prior
+    return out
+
+def gendats3(n,dim=2,delta=2.):
+    N = genclass(n,[1./3,1./3,1./3])
+    x0 = numpy.random.randn(N[0],dim)
+    x1 = numpy.random.randn(N[1],dim)
+    x2 = numpy.random.randn(N[2],dim)
+    x0[:,0] -= delta
+    x1[:,0] += delta
+    x2[:,1] += delta
+    x = numpy.concatenate((x0,x1,x2),axis=0)
+    y = genlab(N,(1,2,3))
+    out = prdataset(x,y)
+    out.name = 'Simple dataset'
+    out.prior = [1./3,1./3,1./3]
+    return out
+
+
 
