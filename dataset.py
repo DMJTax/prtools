b649fcf24a07aaafa642776a2a1befa65e334e57
D.M.J. Tax <davidt@tud0035369.ws.tudelft.net>
2019-04-02 15:04:55 +0200

    Split the prtools again in more manageable pieces.

---
+++ dataset.py
@@ -0,0 +1,275 @@
+"""
+Pattern Recognition Dataset class
+
+Should provide a simple and consistent way to deal with datasets.
+"""
+
+import numpy
+import copy
+
+import matplotlib.pyplot as plt
+
+# === prdataset ============================================
+class prdataset(object):
+    "Prdataset in python"
+
+    def __init__(self,data,labels=None):
+        if isinstance(data,prdataset):
+            #self = copy.deepcopy(data) # why does this not work??
+            self.__dict__ = data.__dict__.copy()   # sigh..
+            return
+        if not isinstance(data,(numpy.ndarray, numpy.generic)):
+            data = numpy.array(data,ndmin=2)
+            if not isinstance(data,(numpy.ndarray, numpy.generic)):
+                raise ValueError('Data matrix should be a numpy matrix.')
+        if labels is not None:
+            if not isinstance(labels,(numpy.ndarray, numpy.generic)):
+                raise ValueError('Label vector should be a numpy matrix.')
+            if (data.shape[0]!=labels.shape[0]):
+                raise ValueError('Number of labels does not match number of data samples.')
+        else:
+            labels = numpy.zeros(data.shape[0])
+        self.name = ''
+        self.featlab = numpy.arange(data.shape[1])
+        self.setdata(data)
+        self.labels = labels
+        self.weights = []
+        self.prior = []
+        self.user = []
+
+    def __str__(self):
+        sz = self.data.shape
+        if (len(self.name)>0):
+            outstr = "%s %d by %d prdataset" % (self.name,sz[0],sz[1])
+        else:
+            outstr = "%d by %d prdataset" % (sz[0],sz[1])
+        cnt = self.classsizes()
+        nrcl = len(cnt)
+        if (nrcl==0):
+            outstr += " with no labels"
+        elif (nrcl==1):
+            outstr += " with 1 class: [%d]"%sz[0]
+        else:
+            outstr += " with %d classes: [%d"%(nrcl,cnt[0])
+            for i in range(1,nrcl):
+                outstr += " %d"%cnt[i]
+            outstr += "]"
+        return outstr
+
+    def float(self):
+        return self.data.copy()
+    def __pos__(self):
+        return self.float()
+    def __add__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data += other
+        return newd
+    def __sub__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data -= other
+        return newd
+    def __mul__(self,other):
+        #print('prdataset multiplication with right')
+        #print('   self='+str(self))
+        #print('   other='+str(other))
+
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        else:
+            return NotImplemented
+            #raise ValueError("Cannot multiply a prdataset with RHS.")
+        newd.data *= other
+        return newd
+    def __div__(self,other):
+        newd = copy.deepcopy(self)
+        if (isinstance(other,prdataset)):
+            other = other.float()
+        newd.data /= other
+        return newd
+
+    def lablist(self):
+        return numpy.unique(self.labels)
+    def nlab(self):
+        (ll,I) = numpy.unique(self.labels,return_inverse=True)
+        I = numpy.array(I)
+        I = I[:,numpy.newaxis] # python is so terrible..:-(
+        return I
+    def signlab(self,posclass=1):  # what is a good default?
+        ll = self.lablist()
+        if (len(ll)>2):
+            raise ValueError('Labels +-1 only for two-class problems.')
+        lab = self.nlab()
+        if (posclass==0):
+            lab = 1-2.0*lab
+        else:
+            lab = 2.0*lab-1
+        return lab
+    def setdata(self,data):
+        self.data = data
+        self.shape = data.shape
+        if len(self.featlab) != data.shape[1]:
+            self.featlab = ['Feature 0']
+            for i in range(1,data.shape[1]):
+                self.featlab.append('Feature %d'%i)
+        return self
+
+    def classsizes(self):
+        try:
+            (k,count) = numpy.unique(self.labels,return_counts=True)
+        except:
+            ll = numpy.unique(self.labels)
+            count = numpy.zeros((len(ll),1))
+            for i in range(len(ll)):
+                count[i] = numpy.sum(1.*(self.labels==ll[i]))
+        return count
+    def nrclasses(self):
+        ll = numpy.unique(self.labels)
+        return len(ll)
+    def findclass(self,cname):
+        ll = numpy.unique(self.labels)
+        I = numpy.where(ll==cname)
+        return I[0][0]
+
+    def __getitem__(self,key):
+        newd = copy.deepcopy(self)
+        k1 = key[0]
+        k2 = key[1]
+        if isinstance(k1,int):  # fucking python!
+            k1 = range(k1,k1+1)
+        if isinstance(k2,int):  # fucking python!!!!
+            k2 = range(k2,k2+1)
+        newkey = (k1,k2)
+        newfeatlab = newd.featlab[key[1]]
+        if not isinstance(newfeatlab,numpy.ndarray): #DXD why??
+            newfeatlab = numpy.array([newfeatlab]) # GRR Python
+        newd.featlab = newfeatlab
+        newd = newd.setdata(newd.data[newkey])
+        newd.labels = newd.labels[newkey[0]]
+        return newd
+
+    def seldat(self,cl):
+        newd = copy.deepcopy(self)
+        #I = (self.labels==cl).nonzero()
+        I = (self.nlab()==cl).nonzero()
+        return newd[I[0],:]   # grr, this python..
+
+    def getprior(self):
+        if (len(self.prior)>0):
+            return self.prior
+        sz = self.classsizes()
+        return sz/float(numpy.sum(sz))
+
+    def concatenate(self,other):
+        out = copy.deepcopy(self)
+        out = out.setdata(numpy.concatenate((out.data,other.data),axis=0))
+        out.labels = numpy.concatenate((out.labels,other.labels),axis=0)
+        return out
+
+
+
+
+# === useful functions =====================================
+def scatterd(a):
+    clrs = a.nlab().flatten()
+    sz = a.data.shape
+    if (sz[1]>1):
+        plt.scatter(a.data[:,0],a.data[:,1],c=clrs)
+        ylab = a.featlab[1]
+    else:
+        plt.scatter(a.data[:,0],numpy.zeros((sz[0],1)),c=clrs)
+        ylab = ''
+    plt.title(a.name)
+    plt.xlabel('Feature '+str(a.featlab[0]))
+    plt.ylabel('Feature '+str(ylab))
+    plt.winter()
+
+def scatterr(a):
+    plt.scatter(a.data[:,0],a.labels)
+    plt.title(a.name)
+    plt.xlabel('Feature '+str(a.featlab[0]))
+    plt.ylabel('Target')
+    plt.winter()
+
+# === datasets ===============================
+def genclass(n,p):
+    "Generate class frequency distribution"
+    if isinstance(n,int):
+        n = [n]  # make a list
+    n = numpy.asarray(n)
+    if (len(n)>1):
+        return n
+    if (len(n.shape)>1):
+        return n
+    p = numpy.asarray(p)
+    if (numpy.abs(numpy.sum(p)-1)>1e-9):
+        raise ValueError('Probabilities do not add up to 1.')
+    c = len(p)
+    P = numpy.cumsum(p)
+    P = numpy.concatenate((numpy.zeros(1),P)) # I hate python
+    z = numpy.zeros(c,dtype=int)
+    x = numpy.random.rand(numpy.sum(n))
+    for i in range(0,c):
+        z[i] = numpy.sum((x>P[i]) & (x<P[i+1]))
+    return z
+
+def genlab(n,lab):
+    if (len(n)!=len(lab)):
+        raise ErrorValue('Number of values in N should match number in lab')
+    out = numpy.tile(lab[0],[n[0],1])
+    for i in range(1,len(n)):
+        out=numpy.concatenate((out,numpy.tile(lab[i],[n[i],1])))
+    return out
+
+def gendat(x,n,seed=None):
+    nrcl = x.nrclasses()
+    clsz = x.classsizes()
+    prior = x.getprior()
+    # and all the casting:
+    if isinstance(n,float):  # we start with a fraction
+        n = n*clsz
+    if isinstance(n,int):    # we start with a total number
+        n = genclass(n,prior)
+    if isinstance(n,tuple):
+        n = list(n)
+    if isinstance(n[0],float) and (n[0]<1.): # a vector/list of fractions
+        n = n*clsz
+    # take care for the seed:
+    numpy.random.seed(seed)
+    # now generate the data:
+    i=0  # first class is special:
+    x1 = x.seldat(i)
+    if (n[i]==clsz[i]):
+        # take a bootstrap sample:
+        I = numpy.random.randint(0,n[i],n[i])
+    elif (n[i]<clsz[i]):
+        I = numpy.random.permutation(range(clsz[i]))
+        I = I[0:int(n[i])]
+    else:
+        I = numpy.random.randint(clsz[i],size=int(n[i]))
+    out = x1[I,:]
+    allI = range(clsz[i])
+    J = numpy.setdiff1d(allI,I)
+    leftout = x1[J,:]
+    # now the other classes:
+    for i in range(1,nrcl):
+        xi = x.seldat(i)
+        if (n[i]==clsz[i]):
+            # take a bootstrap sample:
+            I = numpy.random.randint(0,n[i],n[i])
+        elif (n[i]<clsz[i]):
+            I = numpy.random.permutation(range(clsz[i]))
+            I = I[0:int(n[i])]
+        else:
+            I = numpy.random.randint(clsz[i],size=int(n[i]))
+        out = out.concatenate(xi[I,:])
+        allI = range(clsz[i])
+        J = numpy.setdiff1d(allI,I)
+        leftout = leftout.concatenate(xi[J,:])
+
+    return out,leftout
+
---
+++ mapping.py
@@ -0,0 +1,292 @@
+"""
+Pattern Recognition Mapping class
+
+Should provide a uniform and consistent way of defining transformation for datasets.
+"""
+
+import numpy
+import copy
+import matplotlib.pyplot as plt
+
+from dataset import prdataset
+
+# === prmapping ============================================
+class prmapping(object):
+    "Prmapping in Python"
+
+    def __init__(self,mapping_func,x=[],hyperp=[]):
+        # exception: when only hyperp are given
+        if (not isinstance(x,prdataset)) and (not hyperp): 
+            hyperp = x
+            x = []
+        self.mapping_func = mapping_func
+        self.mapping_type = "untrained"
+        self.name, self.hyperparam = self.mapping_func("untrained",hyperp)
+        self.data = () 
+        self.labels = ()
+        self.shape = [0,0]
+        self.user = []
+        if isinstance(x,prdataset):
+            self = self.train(copy.deepcopy(x))
+
+    def __repr__(self):
+        return "prmapping("+self.mapping_func.func_name+","+self.mapping_type+")"
+    def __str__(self):
+        outstr = ""
+        if (len(self.name)>0):
+            outstr = "%s, " % self.name
+        if (self.mapping_type=="untrained"):
+            outstr += "untrained mapping"
+        elif (self.mapping_type=="trained"):
+            outstr += "%d to %d trained mapping" % (self.shape[0],self.shape[1])
+        else:
+            raise ValueError('Mapping type is not defined.')
+        return outstr
+
+    def shape(self,I=None):
+        if I is not None:
+            if (I==0):
+                return self.shape[0]
+            elif (dim==1):
+                return self.shape[1]
+            else:
+                raise ValueError('Only dim=0,1 are possible.')
+        else:
+            return self.shape
+
+    def init(self,mappingfunc,**kwargs):
+        self.mapping_func = mappingfunc
+        self.mapping_type = "untrained"
+        self.hyperparam = kwargs
+        self.name,self.hyperparam = self.mapping_func('untrained',kwargs)
+        return self
+
+    def train(self,x,args=None):
+        # train
+        if (self.mapping_type=="trained"):
+            raise ValueError('The mapping is already trained and will be retrained.')
+        # maybe the supplied parameters overrule the stored ones:
+        if args is not None:
+            self.hyperparam = args
+        #if (len(self.hyperparam)==0):
+        #    self.data,self.labels = self.mapping_func('train',x)
+        #else:
+        self.data,self.labels = self.mapping_func('train',x,self.hyperparam)
+
+        self.mapping_type = 'trained'
+        # set the input and output sizes 
+        if (hasattr(x,'shape')):  # combiners do not eat datasets
+            self.shape[0] = x.shape[1]
+            # and the output size?
+            xx = +x[:1,:]   # hmmm??
+            out = self.mapping_func("eval",xx,self)
+            self.shape[1] = out.shape[1]
+        return self
+
+    def eval(self,x):
+        # evaluate
+        if (self.mapping_type=="untrained"):
+            raise ValueError('The mapping is not trained and cannot be evaluated.')
+        # not a good idea to supply the true labels?
+        # but it is needed for testc!
+        #if isinstance(x,prdataset):
+        #    x_nolab = copy.deepcopy(x)
+        #    x_nolab.labels = ()
+        #    out = self.mapping_func("eval",x_nolab,self)
+        #else:
+        newx = copy.deepcopy(x)
+        out = self.mapping_func("eval",newx,self)
+        if ((len(self.labels)>0) and (out.shape[1] != len(self.labels))):
+            print(out.shape)
+            print(self.labels)
+            print(len(self.labels))
+            raise ValueError('Output of mapping does not match number of labels.')
+        if isinstance(x,prdataset):
+            newx.setdata(+out)
+        else:
+            newx = out
+        if (len(self.labels)>0):
+            if not isinstance(x,prdataset):
+                newx = prdataset(newx)
+            newx.featlab = self.labels
+        return newx
+
+    def __call__(self,x):
+        if (self.mapping_type=="untrained"):
+            # train
+            out = self.train(x)
+            return out
+        elif (self.mapping_type=="trained"):
+            # evaluate
+            out = self.eval(x)
+            return out
+        else:
+            print(self.mapping_type)
+            raise ValueError('Mapping type is not defined.')
+        return 
+
+    def __mul__(self,other):
+        #print('prmapping multiplication with right')
+        #print('   self='+str(self))
+        #print('   other='+str(other))
+        if (isinstance(other,prmapping)):
+            # we get a sequential mapping
+            leftm = copy.deepcopy(self)
+            rightm = copy.deepcopy(other)
+            # avoid the constructor of prmapping: the constructor will always
+            # return an 'untrained' mapping, while it *might* be possible to
+            # get a trained one when the two input mappings are already
+            # trained:
+            out = sequentialm((leftm,rightm))
+            return out
+        else:
+            raise ValueError('Prmapping times something not defined.')
+
+    def __rmul__(self,other):
+        if (isinstance(other,prdataset)):
+            newself = copy.deepcopy(self)
+            return newself(other)
+        else:
+            return NotImplemented
+
+
+def sequentialm(task=None,x=None,w=None):
+    "Sequential mapping"
+    if not isinstance(task,str):
+        # we should have gotten a list of two prmappings
+        if not isinstance(task,tuple):
+            raise ValueError('Sequential map expects a list of 2 prmappings.')
+        if not isinstance(task[0],prmapping):
+            raise ValueError('Sequential map expects a list of 2 prmappings.')
+        if not isinstance(task[1],prmapping):
+            raise ValueError('Sequential map expects a list of 2 prmappings.')
+        newm = copy.deepcopy(task)
+        # if both mappings are trained, the sequential mapping is also trained!
+        # (this is an exception to the standard, where you need data in order to
+        # train a prmapping)
+        if (newm[0].mapping_type=='trained') and \
+                (newm[1].mapping_type=='trained'):
+            # now the seq.map is already trained:
+            if (newm[0].shape[1] != 0) and (newm[1].shape[0] !=0) and \
+                    (newm[0].shape[1] != newm[1].shape[0]):
+                raise ValueError('Output size map1 does not match input size map2.')
+            # do the constructor, but make sure that the hyperparameters are None:
+            w = prmapping(sequentialm,None)
+            w.data = newm
+            w.shape[0] = newm[0].shape[0]
+            w.shape[1] = newm[1].shape[1]
+            if (len(newm[1].labels)==0) and (newm[1].shape[1]==0):
+                # the second mapping does not have labels and sizes defined:
+                w.labels = newm[0].labels
+            else:
+                w.labels = newm[1].labels
+            w.mapping_type = 'trained'
+            w.name = newm[0].name+'+'+newm[1].name
+            return w
+        else:
+            if x is None:
+                w = prmapping(sequentialm,newm,x)
+            else:
+                newx = copy.deepcopy(x)
+                w = prmapping(sequentialm,newm,newx)
+            w.name = newm[0].name+'+'+newm[1].name
+            return w
+    if (task=='untrained'):
+        # just return the name, and hyperparameters
+        if x is None:
+            mapname = 'Sequential'
+        else:
+            mapname = x[0].name+'+'+x[1].name
+        return mapname, x
+    elif (task=="train"):
+        # we are going to train the mapping
+        u = copy.deepcopy(w)  # I hate Python..
+        x1 = copy.deepcopy(x) # Did I say that I hate Python??
+        if (u[0].mapping_type=='untrained'):
+            neww = u[0].train(x1)
+            u = (neww, u[1])
+        newx = copy.deepcopy(x1)
+        x2 = u[0](newx)
+        if (u[1].mapping_type=='untrained'):
+            neww = u[1].train(x2)
+            u = (u[0],neww)
+        # fix the labels:
+        if (len(u[1].labels)==0) and (u[1].shape[1]==0):
+            # the second mapping does not have labels and sizes defined:
+            labels = u[0].labels
+        else:
+            labels = u[1].labels
+        return u, labels
+    elif (task=="eval"):
+        # we are applying to new data
+        W = w.data   # get the parameters out
+        return W[1](W[0](x))
+    else:
+        print(task)
+        raise ValueError('This task is *not* defined for sequentialm.')
+
+# === useful functions =====================================
+
+def plotc(f,levels=[0.0],colors=None,gridsize = 30):
+    ax = plt.gca()
+    if colors is None:
+        colors = next(ax._get_lines.prop_cycler)['color']
+    xl = ax.get_xlim()
+    yl = ax.get_ylim()
+    dx = (xl[1]-xl[0])/(gridsize-1)
+    dy = (yl[1]-yl[0])/(gridsize-1)
+    x = numpy.arange(xl[0],xl[1]+0.01*dx,dx)
+    y = numpy.arange(yl[0],yl[1]+0.01*dy,dy)
+    X0,X1 = numpy.meshgrid(x,y)
+    X0.shape = (gridsize*gridsize, 1)
+    X1.shape = (gridsize*gridsize, 1)
+    dat = numpy.hstack((X0,X1))
+    if (f.shape[0]>2):
+        print("PLOTC: Mapping is >2D, set remaining inputs to 0.")
+        X2 = numpy.zeros((gridsize*gridsize,f.shape[0]-2))
+        dat = numpy.hstack((dat,X2))
+
+    out = +f(dat)
+    for i in range(1,out.shape[1]):
+        otherout = copy.deepcopy(out)
+        otherout[:,i] = -numpy.inf
+        z = out[:,i] - numpy.amax(otherout,axis=1)
+        z.shape = (gridsize,gridsize)
+        plt.contour(x,y,z,levels,colors=colors)
+
+def plotm(f,nrlevels=10,colors=None,gridsize = 30):
+    ax = plt.gca()
+    if colors is None:
+        colors = next(ax._get_lines.prop_cycler)['color']
+    xl = ax.get_xlim()
+    yl = ax.get_ylim()
+    dx = (xl[1]-xl[0])/(gridsize-1)
+    dy = (yl[1]-yl[0])/(gridsize-1)
+    x = numpy.arange(xl[0],xl[1]+0.01*dx,dx)
+    y = numpy.arange(yl[0],yl[1]+0.01*dy,dy)
+    X0,X1 = numpy.meshgrid(x,y)
+    X0.shape = (gridsize*gridsize, 1)
+    X1.shape = (gridsize*gridsize, 1)
+    dat = numpy.hstack((X0,X1))
+    if (f.shape[0]>2):
+        print("PLOTM: Mapping is >2D, set remaining inputs to 0.")
+        X2 = numpy.zeros((gridsize*gridsize,f.shape[0]-2))
+        dat = numpy.hstack((dat,X2))
+
+    out = +f(dat)
+    for i in range(out.shape[1]):
+        z = out[:,i]
+        levels = numpy.linspace(numpy.min(z),numpy.max(z),nrlevels)
+        z.shape = (gridsize,gridsize)
+        plt.contour(x,y,z,levels,colors=colors)
+
+def plotr(f,color=None,gridsize=100):
+    ax = plt.gca()
+    if color is None:
+        color = next(ax._get_lines.prop_cycler)['color']
+    xl = ax.get_xlim()
+    dx = (xl[1]-xl[0])/(gridsize-1)
+    x = numpy.arange(xl[0],xl[1]+0.01*dx,dx)
+    xx = prdataset(x[:,numpy.newaxis])
+    y = +f(xx)
+    plt.plot(x,y,color=color)
