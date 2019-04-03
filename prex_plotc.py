7bb2b82a1501798901b6a7a1ecef8da0b3164225
D.M.J. Tax <davidt@TUD0035368.fritz.box>
2019-04-03 14:41:13 +0200

    Slightly beautified the example.

--- prex_plotc.py
+++ prex_plotc.py
@@ -1,28 +1,31 @@
-from prtools import *
+import prtools as pr
 import matplotlib.pyplot as plt
 
 # standard prtools: make data and define classifier:
-#a = gendatb((30,30))
-#a = gendatd((30,30))
-a = gendath((30,30))
-#a = gendats3((100))
+a = pr.gendatb((30,30))
+#a = pr.gendatd((30,30))
+#a = pr.gendath((30,30))
+#a = pr.gendats3((100))
+print(a)
 
-#u = nmc()
-#u = scalem()*proxm(('rbf',0.5))*nmc()
-u = gaussm(('full',0.1))*bayesrule()
-#u = qdc()
-#u = knnc([1])
-#u = scalem()*parzenc()
+#u = pr.nmc()
+#u = pr.scalem()*pr.proxm(('rbf',0.5))*pr.nmc()
+u = pr.gaussm(('full',0.1))*pr.bayesrule()
+#u = pr.ldc()
+#u = pr.knnc([1])
+#u = pr.scalem()*pr.parzenc()
 
-# train classifier, plot:
+# train classifier, get labels, error and plot:
 w = a*u
+print(w)
 pred = a*w
-print w
-err = pred*testc()
+print(pred)
+lab = pred*pr.labeld()
+print(+lab)
+err = pr.testc(pred)
 print('Classification error: %3.2f' % err)
 plt.figure(1)
-scatterd(a)
-plotc(w)
-#plotm(w,gridsize=100,colors='gray')
+pr.scatterd(a)
+pr.plotc(w)
 
 plt.show()
