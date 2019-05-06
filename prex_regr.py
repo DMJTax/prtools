2c0a097428f74fec552ee0813d3a47dc84d27024
D.M.J. Tax <davidt@tud0035369.ws.tudelft.net>
2019-05-06 15:38:17 +0200

    Give more explanation.

--- prex_regr.py
+++ prex_regr.py
@@ -3,12 +3,16 @@ from prtools import *
 import matplotlib.pyplot as plt
 import uci
 
+# Read in one of the UCI datasets (and use the class labels as a
+# regression target):
 a = uci.breast(True)
 a = uci.missingvalues(a,'remove')
 
-for lamb in (0.1, 1.0, 10.0):
-    w = lassor(a,lamb)
-    print("Setting lambda to %f gives the weights:"%lamb)
+# Use a few regularisation parameters and see how sparse the solutions
+# become:
+for alpha in (0.1, 1.0, 10.0):
+    w = lassor(a,alpha)
+    print("Setting alpha=%f gives the weights:"%lamb)
     print(w.data.coef_)
     err = a*w*testr()
     print("MSE = %f" % err)
