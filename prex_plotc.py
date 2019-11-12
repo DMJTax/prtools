91711abdd72837c52499ac56cbb066f6cb6c5e3b
D.M.J. Tax <davidt@tud0035369.ws.tudelft.net>
2019-11-12 12:53:43 +0100

    A few more examples.

--- prex_plotc.py
+++ prex_plotc.py
@@ -10,10 +10,11 @@ print(a)
 
 #u = pr.nmc()
 #u = pr.scalem()*pr.proxm(('rbf',0.5))*pr.nmc()
-u = pr.gaussm(('full',0.1))*pr.bayesrule()
+#u = pr.gaussm(('full',0.1))*pr.bayesrule()
 #u = pr.ldc()
 #u = pr.knnc([1])
 #u = pr.scalem()*pr.parzenc()
+u = pr.scalem()*pr.parzenc([0.6])
 
 # train classifier, get labels, error and plot:
 w = a*u
