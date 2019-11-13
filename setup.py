6f9fa426298ca62ab64ec4cad1f34c7df6924d17
Martijn Janssen <martijn9612+github@gmail.com>
2019-11-13 13:20:56 +0100

    add setup file to allow python install commands

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='prtools',
+   version='1.0',
+   description='Bare-bones implementation of Prtools for Python',
+   author='D.M.J. Tax',
+   author_email='',
+   packages=[],
+   install_requires=['sklearn', 'numpy', 'matplotlib'],
+)
