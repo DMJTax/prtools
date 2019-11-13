9cc075c1fe981e1ae780f457f3d1f957651ea834
Toine Hartman <tjbhartman@gmail.com>
2019-11-13 14:15:13 +0100

    Correct import configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='prtools',
-   version='1.0',
+   version='1.1',
    description='Bare-bones implementation of Prtools for Python',
    author='D.M.J. Tax',
    author_email='',
