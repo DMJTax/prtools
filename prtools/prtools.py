472a0b5bc813c5053a15d8336acd36593eb75ef3
Daniel Kappelle <daniel.kappelle@gmail.com>
2019-12-17 08:33:23 +0100

    Bugfix, fix ridger dimension (#12)

--- prtools/prtools.py
+++ prtools/prtools.py
@@ -1549,7 +1549,7 @@ def ridger(task=None,x=None,w=None):
         # we are going to train the mapping
         n,dim = x.shape
         dat = numpy.hstack((+x,numpy.ones((n,1))))
-        Sinv = numpy.linalg.inv(dat.T.dot(dat) + w*numpy.eye(dim))
+        Sinv = numpy.linalg.inv(dat.T.dot(dat) + w*numpy.eye(dim+1))
         beta = Sinv.dot(dat.T).dot(x.targets)
         # store the parameters, and labels:
         return beta,['target']
